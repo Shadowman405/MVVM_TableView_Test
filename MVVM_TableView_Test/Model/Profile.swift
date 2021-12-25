4ee91474faeb6a9c7cd7b72f743b1a9c303e8215
Maxim Mitin <maximus405@gmail.com>
2021-12-25 22:04:26 +0300

    Preparing for MVVM refactoring , simple TableView App

---
+++ MVVM_TableView_Test/Model/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM_TableView_Test
+//
+//  Created by Maxim Mitin on 25.12.21.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
---
+++ MVVM_TableView_Test/View/TableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.swift
+//  MVVM_TableView_Test
+//
+//  Created by Maxim Mitin on 25.12.21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var fullNameLbl: UILabel!
+    @IBOutlet weak var ageLbl: UILabel!
+
+}
